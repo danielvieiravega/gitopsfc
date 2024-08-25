0f75d4564501ea153c622b7c42006a17f18e45e5
Daniel Vieira Vega <daniel.vega@me.com.br>
2024-08-25 19:06:50 -0300

    alterado retorno

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>hello argo </h1>"))
+		w.Write([]byte("<h1>hello argo novamente </h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@ kind create cluster --name=gitopsfc
 
 argocd admin initial-password -n argocd
 kubectl port-forward svc/argocd-server -n argocd 8080:443
+ kubectl port-forward service/goserver-service 8081:8080
+
+
+ https://argoproj.github.io/cd/
